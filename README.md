642bcef8f2469fee35454ceed8ea5cebe92439e8
mk10969 <mk10969@gmail.com>
2021-04-12 18:39:52 +0900

    add modarn-shell

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 playbook become=yesは、sudoの意味。
 
 
-- test command
-```
-ansible-playbook -i inventories/hosts playbooks/test.yaml
-```
+- ping command  
 
+cat ~/inventories/hosts 
+で標準出力されたhostに対して、接続可能かpingを打つ。
+
+```
+$ ansible-playbook -i inventories/hosts playbooks/ping.yaml
+```
---
+++ roles/modern-shell/files/config.fish
@@ -0,0 +1,26 @@
+### env path
+
+# k8s
+set -x KUBECONFIG $HOME/.kube/config
+
+# set -x LANGUAGE ja_JP.UTF-8
+# set -x LC_ALL ja_JP.UTF-8
+# set -x LANG ja_JP.UTF-8
+# set -x LC_TYPE ja_JP.UTF-8
+
+# # golang
+# set -x GOPATH $HOME/go
+# set -x PATH $GOPATH/bin $PATH
+
+# # Kustomize
+# set -x PATH $HOME/kustomize $PATH
+
+### theme
+starship init fish | source
+
+### alias
+alias ll='ls -la'
+alias lt='ls -ltra'
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
